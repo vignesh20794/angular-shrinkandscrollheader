1b4cf387ca3a2cc29be341f78a17e772628c115f
vignesh20794 <kumar.vignesh752@gmail.com>
2019-05-02 16:40:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-shrinkandscrollheader
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-shrinkandscrollheader)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+}
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "60px";
+  }
+
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
